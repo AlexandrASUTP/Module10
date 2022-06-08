0c830a9356475be505bcfc7f5b7de7d64e0f8738
Name <email@example.ru>
2022-06-09 00:00:43 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate calc = new Calculate();
+            try
+            {
+                Console.Write("Число а:");
+                int a = int.Parse(Console.ReadLine());
+                Console.Write("Число b:");
+                int b = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Сумма = {((IAdd)calc).Add(a, b)}"); ;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Не верный формат значения");
+            }
+            finally
+            {
+                Console.WriteLine("Калькуляция завершена");
+                Console.ReadKey();
+            }
+
+        }
+    }
+    public interface IAdd
+    {
+        public int Add(int a, int b);
+    }
+    public class Calculate : IAdd
+    {
+
+        int IAdd.Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Task2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var Logger = new Logger();
+            var calc = new Calculate();
+
+            
+            try
+            {
+                Console.Write("Число а:");
+                int a = int.Parse(Console.ReadLine());
+                Console.Write("Число b:");
+                int b = int.Parse(Console.ReadLine());
+                Console.ReadKey();
+                Logger.Event("Вычисление началось");
+                Console.WriteLine($"Сумма = {((IAdd)calc).Add(a, b)}");
+                Console.ReadKey();
+            }
+            catch (FormatException)
+            {
+                Logger.Error("Не верный формат значения");
+                
+            }
+            finally
+            {
+                Logger.Event("Калькуляция завершена");
+                Console.ReadKey();
+            }
+
+        }
+    }
+    public interface IAdd
+    {
+        public int Add(int a, int b);
+        
+    }
+    public class Calculate : IAdd
+    {
+       // ILogger Logger { get; }
+      
+        int IAdd.Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+    public interface ILogger
+    {
+        void Event(string massege);
+        void Error(string massege);
+    }
+    
+    public class Logger : ILogger
+    {
+        public void Error(string massege)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(massege);
+        }
+        public void Event(string massege)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(massege);
+        }
+    }
+
+}
